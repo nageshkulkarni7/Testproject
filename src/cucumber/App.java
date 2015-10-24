b9e8c919d4560ab1b59e9856d924ed9fd9872511
nagesh <nagesh@192.168.1.5>
2015-10-25 01:45:32 +0530

    first addition

--- src/cucumber/App.java
+++ src/cucumber/App.java
@@ -5,6 +5,7 @@ public class App {
 	public static void main (String args[]){
 		System.out.println("Print line");
 		System.out.println("git store");
+		System.out.println("first o/p added");
 	}
 
 }
